769c025246d328c777fcb04ebdc5f39768f8ae6f
A7med-p <47med.aa3@gmail.com>
2025-07-10 16:32:02 +0300

    adding the enter button now we can play the game :)

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 let squareid = 0;
 let squarepid = 0;
 let userword=[]
+let theChosenOne = "CLEAN"
+let one = theChosenOne.split('')
+let count = 0;
 
 let letters = document.querySelectorAll('.sqrdown');
 let square = document.querySelectorAll('.sqrup');
+let enter = document.querySelector('.enter')
 
 letters.forEach(letter => {
  letter.addEventListener('click', () => {
@@ -15,4 +19,26 @@ letters.forEach(letter => {
         console.log(userword);
     }
 })
+})
+
+enter.addEventListener('click',()=>{
+if (squareid == 5) {
+
+ for ( var i = 0 ; i < 5; i++) {
+   for ( var j = 0 ; j < 5; j++) {
+            
+     if (userword[i] == one[j]) {
+        square[count].style.backgroundColor = 'yellow'
+     }
+     if (userword[i] == one[i]) {   
+        square[count].style.backgroundColor = 'lightgreen'
+     }
+    }
+    count++
+    }
+    squareid = 0
+    for (let i = 0; i < 5; i++) {
+        userword.pop()  
+    }
+}
 })
