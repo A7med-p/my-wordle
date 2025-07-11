909cb14eaf6d37a16a3135c0926f154fb07e135c
A7med-p <47med.aa3@gmail.com>
2025-07-11 18:20:04 +0300

    entering by keyboard

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ let letters = document.querySelectorAll('.sqrdown');
 let square = document.querySelectorAll('.sqrup');
 let enter = document.querySelector('.enter')
 let back = document.querySelector('.back')
+let keyboards = document.querySelectorAll('body')
 
 letters.forEach(letter => {
  letter.addEventListener('click', () => {
@@ -31,6 +32,198 @@ letters.forEach(letter => {
 }
 })
 })
+
+keyboards.forEach(keyboard => {
+keyboard.addEventListener('keyup', (input) => {
+    console.log('click')
+let e;
+switch (input.key) {
+    case ('a') :
+        e = "A"
+        break;
+    case ('b') :
+        e = "B"
+        break;
+    case ('c') :
+        e = "C"
+        break;
+    case ('d') :
+        e = "D"
+        break;
+    case ('e') :
+        e = "E"
+        break;
+    case ('f') :
+        e = "F"
+        break;
+    case ('g') :
+        e = "G"
+        break;
+    case ('h') :
+        e = "H"
+        break;
+    case ('i') :
+        e = "I"
+        break;
+    case ('j') :
+        e = "J"
+        break;
+    case ('k') :
+        e = "K"
+        break;
+    case ('l') :
+        e = "L"
+        break;
+    case ('m') :
+        e = "M"
+        break;
+    case ('n') :
+        e = "N"
+        break;
+    case ('o') :
+        e = "O"
+        break;
+    case ('p') :
+        e = "P"
+        break;
+    case ('q') :
+        e = "Q"
+        break;
+    case ('r') :
+        e = "R"
+        break;
+    case ('s') :
+        e = "S"
+        break;
+    case ('t') :
+        e = "T"
+        break;
+    case ('u') :
+        e = "U"
+        break;
+    case ('v') :
+        e = "V"
+        break;
+    case ('w') :
+        e = "W"
+        break;
+    case ('x') :
+        e = "X"
+        break;
+    case ('y') :
+        e = "Y"
+        break;
+    case ('z') :
+        e = "Z"
+        break;
+    case ('A') :
+        e = "A"
+        break;
+    case ("B") :
+        e = "B"
+        break;
+    case ("C") :
+        e = "C"
+        break;
+    case ("D") :
+        e = "D"
+        break;
+    case ("E") :
+        e = "E"
+        break;
+    case ("F") :
+        e = "F"
+        break;
+    case ("G") :
+        e = "G"
+        break;
+    case ("H") :
+        e = "H"
+        break;
+    case ("I") :
+        e = "I"
+        break;
+    case ("J") :
+        e = "J"
+        break;
+    case ("K") :
+        e = "K"
+        break;
+    case ("L") :
+        e = "L"
+        break;
+    case ("M") :
+        e = "M"
+        break;
+    case ("N") :
+        e = "N"
+        break;
+    case ("O") :
+        e = "O"
+        break;
+    case ("P") :
+        e = "P"
+        break;
+    case ("Q") :
+        e = "Q"
+        break;
+    case ("R") :
+        e = "R"
+        break;
+    case ("S") :
+        e = "S"
+        break;
+    case ("T") :
+        e = "T"
+        break;
+    case ("U") :
+        e = "U"
+        break;
+    case ("V") :
+        e = "V"
+        break;
+    case ("W") :
+        e = "W"
+        break;
+    case ("X") :
+        e = "X"
+        break;
+    case ("Y") :
+        e = "Y"
+        break;
+    case ("Z") :
+        e = "Z"
+        break;
+    case ("Enter") :
+        e = "Enter"
+        break;
+    case ("Backspace") :
+        e = "Backspace"
+        break;
+    default:
+        e = " "
+        break;
+}
+if(attempts < maxattempts){
+
+    if (squareid <= 4 && e !== 'Enter' && e !== 'Backspace' && e !== ' ') {
+        square[squarepid].innerText = e;
+        userword.push(e)
+        squareid = squareid + 1;
+        squarepid = squarepid + 1;
+    }
+
+    if( e == 'Enter'&& squareid == 5){
+        enters()
+    }
+    if (e == 'Backspace' && attempts !== maxattempts) {
+        backs()
+        
+    }
+}
+})
+})
+
 
 function enters() {
 
